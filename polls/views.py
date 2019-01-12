f7baec1eab2a00dd107698f2c4c584e7aefc8f3b
Lewis Buckley <lewis@lewisbuckley.co.uk>
2019-01-12 21:22:19 +0000

    First python commit

---
+++ polls/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+
+from .models import Question
+
+def index(request):
+    questions = Question.objects.values_list('question_text', flat=True)
+    output = ', '.join(questions)
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
